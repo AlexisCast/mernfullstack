ba2ef0c88e5c2c423072697fe2afb0b5fc471335
Alexis Castellanos <intelaki@gmail.com>
2020-07-12 19:02:16 -0500

    'UserItem', 'Users' & 'UserList' created

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Users=()=> {
-    return <h2>Users</h2>
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+	const USERS = [
+		{
+			id: "u1",
+			name: "Alexis Castellanos",
+			image:
+				"https://vignette.wikia.nocookie.net/catscratch/images/0/03/Gomez.png/revision/latest?cb=20180104165830",
+			places: 3,
+		},
+	];
+
+	return <UsersList items={USERS} />;
 };
 export default Users;
