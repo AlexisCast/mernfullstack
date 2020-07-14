2b49d98ff75de151e9eab9b520bc4eb5f195821b
Alexis Castellanos <intelaki@gmail.com>
2020-07-13 21:54:44 -0500

    'NavLinks' added for desktop

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import MainHeader from './MainHeader';
-import './MainNavigation.css';
+import MainHeader from "./MainHeader";
+import NavLInks from "./NavLinks";
+import "./MainNavigation.css";
 
-const MainNavigation = props => {
-  return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-        ...
-      </nav>
-    </MainHeader>
-  );
+const MainNavigation = (props) => {
+	return (
+		<MainHeader>
+			<button className="main-navigation__menu-btn">
+				<span />
+				<span />
+				<span />
+			</button>
+			<h1 className="main-navigation__title">
+				<Link to="/">YourPlaces</Link>
+			</h1>
+			<nav className="main-navigation__header-nav ">
+				<NavLInks />
+			</nav>
+		</MainHeader>
+	);
 };
 
 export default MainNavigation;
