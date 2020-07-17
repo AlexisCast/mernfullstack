2434e45c2826f7ccdc20d5c2b6d81176b2229617
Alexis Castellanos <intelaki@gmail.com>
2020-07-16 21:48:31 -0500

    finishing the ADD PLACE in 'NewPlace' component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -50,6 +50,7 @@ const NewPlace = () => {
 		},
 		isValid: false,
 	});
+
 	const inputHandler = useCallback((id, value, isValid) => {
 		dispatch({
 			type: "INPUT_CHANGE",
@@ -58,9 +59,15 @@ const NewPlace = () => {
 			inputId: id,
 		});
 	}, []);
-    console.log("formState", formState);
+
+	const placeSubmitHandler = (event) => {
+		event.preventDefault();
+		console.log("formState", formState);
+		console.log("formState.inputs", formState.inputs);
+	};
+
 	return (
-		<form className="place-form">
+		<form className="place-form" onSubmit={placeSubmitHandler}>
 			<Input
 				id="title"
 				element="input"
@@ -77,6 +84,15 @@ const NewPlace = () => {
 				label="Description"
 				validators={[VALIDATOR_MINLENGTH(5)]}
 				errorText="Please enter a valid description at least 5 characteres"
+				onInput={inputHandler}
+			/>
+			<Input
+				id="address"
+				element="input"
+				type="text"
+				label="Address"
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText="Please enter a valid address"
 				onInput={inputHandler}
 			/>
 			<Button type="submit" disabled={!formState.isValid}>
