9dcde5c8cb74b368d4f0885e9141ef0a053e65ba
Alexis Castellanos <intelaki@gmail.com>
2020-07-17 18:51:35 -0500

    fixing minor issues

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
 import {
 	VALIDATOR_REQUIRE,
 	VALIDATOR_MINLENGTH,
@@ -27,7 +28,7 @@ const DUMMY_PLACES = [
 	},
 	{
 		id: "p2",
-		title: "Empire State Building",
+		title: "Emp. State Building",
 		description: "One of the most famous sky scrapers in the world!",
 		imageUrl:
 			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
@@ -61,19 +62,21 @@ const UpdatePlace = () => {
 	const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
 	useEffect(() => {
-		setFormatData(
-			{
-				title: {
-					value: identifiedPlace.title,
-					isValid: false,
+		if (identifiedPlace) {
+			setFormatData(
+				{
+					title: {
+						value: identifiedPlace.title,
+						isValid: false,
+					},
+					description: {
+						value: identifiedPlace.description,
+						isValid: false,
+					},
 				},
-				description: {
-					value: identifiedPlace.description,
-					isValid: false,
-				},
-			},
-			true
-		);
+				true
+			);
+		}
 		setIsLoading(false);
 	}, [setFormatData, identifiedPlace]);
 
@@ -85,7 +88,9 @@ const UpdatePlace = () => {
 	if (!identifiedPlace) {
 		return (
 			<div className="center">
-				<h2>Could not find place!</h2>
+				<Card>
+					<h2>Could not find place!</h2>
+				</Card>
 			</div>
 		);
 	}
