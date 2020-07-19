2de6e9d4885fffcc1e19c2a4a7216459cde281b9
Alexis Castellanos <intelaki@gmail.com>
2020-07-19 18:50:22 -0500

    added 'Auth' component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from './user/pages/Auth';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
 					<Route path="/:userId/places" exact component={UserPlaces} />
 					<Route path="/places/new" exact component={NewPlace} />
 					<Route path="/places/:placeId" exact component={UpdatePlace} />
+					<Route path="/auth" exact component={Auth} />
 					<Redirect to="/" />
 				</Switch>
 			</main>
