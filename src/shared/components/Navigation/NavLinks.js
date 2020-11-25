e18f7adb2c6ae7bcb45f95b8c2b4fff7afd633c2
Alexis Castellanos <intelaki@gmail.com>
2020-11-25 01:58:42 -0600

    fixing navlinks

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -7,6 +7,7 @@ import "./NavLinks.css";
 
 const NavLinks = (props) => {
 	const auth = useContext(AuthContext);
+	console.log("auth", auth);
 
 	return (
 		<ul className="nav-links">
@@ -17,7 +18,7 @@ const NavLinks = (props) => {
 			</li>
 			{auth.isLoggedIn && (
 				<li>
-					<NavLink to="/u1/places">MY PLACES</NavLink>
+					<NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
 				</li>
 			)}
 			{auth.isLoggedIn && (
