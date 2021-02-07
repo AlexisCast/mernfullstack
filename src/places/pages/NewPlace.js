686ecfb1a0588a2e862833da8f7001f2f00f9abb
Alexis Castellanos <intelaki@gmail.com>
2021-02-07 10:57:38 -0600

    uploading images for New Places

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,6 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 
 import {
 	VALIDATOR_REQUIRE,
@@ -34,6 +35,10 @@ const NewPlace = () => {
 				value: "",
 				isValid: false,
 			},
+			image: {
+				value: null,
+				isValid: false,
+			},
 		},
 		false
 	);
@@ -45,20 +50,32 @@ const NewPlace = () => {
 		console.log("formState", formState);
 		console.log("formState.inputs", formState.inputs);
 		try {
+			// await sendRequest(
+			// 	"http://localhost:5000/api/places",
+			// 	"POST",
+			// 	JSON.stringify({
+			// 		title: formState.inputs.title.value,
+			// 		description: formState.inputs.description.value,
+			// 		address: formState.inputs.address.value,
+			// 		creator: auth.userId,
+			// 	}),
+			// 	{
+			// 		"Content-Type": "application/json",
+			// 	}
+			// );
+			// //Redirect the user to a different page
+			// history.push("/");
+			const formData = new FormData();
+			formData.append("title", formState.inputs.title.value);
+			formData.append("description", formState.inputs.description.value);
+			formData.append("address", formState.inputs.address.value);
+			formData.append("creator", auth.userId);
+			formData.append("image", formState.inputs.image.value);
 			await sendRequest(
 				"http://localhost:5000/api/places",
 				"POST",
-				JSON.stringify({
-					title: formState.inputs.title.value,
-					description: formState.inputs.description.value,
-					address: formState.inputs.address.value,
-					creator: auth.userId,
-				}),
-				{
-					"Content-Type": "application/json",
-				}
+				formData
 			);
-			//Redirect the user to a different page
 			history.push("/");
 		} catch (err) {}
 	};
@@ -92,6 +109,11 @@ const NewPlace = () => {
 					validators={[VALIDATOR_REQUIRE()]}
 					errorText="Please enter a valid address."
 					onInput={inputHandler}
+				/>
+				<ImageUpload
+					id="image"
+					onInput={inputHandler}
+					errorText="Please provide an image."
 				/>
 				<Button type="submit" disabled={!formState.isValid}>
 					ADD PLACE
